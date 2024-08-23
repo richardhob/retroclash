ec9e5eea6acec0811e8a3891088095e1180e1242
Ricky <richardhob@gmail.com>
2024-08-22 20:27:59 -0700

    End of Chapter 4 :)

--- basics/src/Retro/Blinky.hs
+++ basics/src/Retro/Blinky.hs
@@ -22,7 +22,7 @@ import Clash.Prelude
 
 import Retro.Util
 
-createDomain vSystem{vName="Dom100", vPeriod=hzToPeriod 100_000_000}
+-- createDomain vSystem{vName="Dom100", vPeriod=hzToPeriod 100_000_000}
 
 data OnOff on off = On (Index on)
                   | Off (Index off)
@@ -36,14 +36,44 @@ countOnOff :: (KnownNat on, KnownNat off) => OnOff on off -> OnOff on off
 countOnOff (On  x) = maybe (Off 0) On  $ succIdx x
 countOnOff (Off y) = maybe (On  0) Off $ succIdx y
 
-blinkingSecond :: forall dom. (HiddenClockResetEnable dom, _) => Signal dom Bit
-blinkingSecond = boolToBit . isOn <$> r
-    where r :: Signal dom (OnOff (ClockDivider dom (Milliseconds 500)) 
-                                 (ClockDivider dom (Milliseconds 500)))
-          r = register (Off 0) $ countOnOff <$> r
+-- blinkingSecond :: forall dom. (HiddenClockResetEnable dom, _) => Signal dom Bit
+-- blinkingSecond = boolToBit . isOn <$> r
+    -- where r :: Signal dom (OnOff (ClockDivider dom (Milliseconds 500)) 
+                                 -- (ClockDivider dom (Milliseconds 500)))
+          -- r = register (Off 0) $ countOnOff <$> r
+ 
+-- topEntity :: "CLK100MHZ" ::: Clock Dom100
+          -- -> "LED" ::: Signal Dom100 Bit
+-- topEntity clk = withClockResetEnable clk resetGen enableGen blinkingSecond
+-- makeTopEntity 'topEntity
 
-topEntity :: "CLK100MHZ" ::: Clock Dom100
-          -> "LED" ::: Signal Dom100 Bit
-topEntity clk = withClockResetEnable clk resetGen enableGen blinkingSecond
+-- Exercises 
+-- 1. Blink only a set number of times
+-- 2. Blink multiple LEDs at different speeds
+-- 3. Sychronized LED Blinking. Write fun patters like a left-to-right then
+--    right-to-left sweep. Or sweep from both sides to the center then outwards
+--    again
+-- 4. Sequence multiple LED blinking patters, each one shown for a couple of
+--    seconds before switching to the next one
+-- exercise1 :: forall dom. (HiddenClockResetEnable dom, _) => Signal dom Bit
+-- exercise1 = boolToBit . isOn <$> blink
+    -- where blink :: Signal dom (OnOff (ClockDivider dom (Milliseconds 500))
+                                     -- (ClockDivider dom (Milliseconds 500)))
+          -- blink = regEn (Off 0) ((< 10) <$> counter) (countOnOff <$> blink)
+-- 
+          -- counter :: Signal dom Int
+          -- counter = x 
+              -- where x = register 0 ((maybeIncrement <$> blink) <*> x)
+                    -- maybeIncrement (On v) = if v == 1 then ((+) 1) else ((+) 0)
+                    -- maybeIncrement (Off _) = ((+) 0)
+
+topEntity :: "CLK"    ::: Clock  System
+          -> "BUTTON" ::: Signal System (Active High)
+          -> "LED"    ::: Signal System (Active High)
+topEntity clk = withClockResetEnable clk resetGen enableGen board
+    where board button  = toActive <$> led
+            where button' = debounce (SNat @(Milliseconds 5)) False (fromActive <$> button)
+                  click = isRising False button'
+                  led = regEn False click (not <$> led)
 
 makeTopEntity 'topEntity
--- basics/src/Retro/Util.hs
+++ basics/src/Retro/Util.hs
@@ -28,10 +28,19 @@ instance IsActive Low where
 type HzToPeriod (freq :: Nat) = 1000000000000 `Div` freq
 type ClockDivider dom ps = ps `Div` DomainPeriod dom
 
+-- Throws an error if greater than the max bound
+--
+-- > (succIdx 38) :: Maybe (Index 40)
+-- Just 39
+-- > (succIdx 39) :: Maybe (Index 40)
+-- Nothing
+-- > (succIdx 40) :: Maybe (Index 40)
+-- *** Exception: X: Clash.Sized.Index: result 40 is out of bounds [0..39]
 succIdx :: (Eq a, Enum a, Bounded a) => a -> Maybe a
 succIdx x | x == maxBound = Nothing
           | otherwise = Just $ succ x
 
+-- Throws an error if less than or equal to the min bound
 predIdx :: (Eq a, Enum a, Bounded a) => a -> Maybe a
 predIdx x | x == minBound = Nothing
           | otherwise = Just $ pred x
@@ -41,3 +50,31 @@ type Milliseconds   (ms :: Nat) = Microseconds (1000 * ms)
 type Microseconds   (us :: Nat) = Nanoseconds  (1000 * us)
 type Nanoseconds    (ns :: Nat) = Picoseconds  (1000 * ns)
 type Picoseconds    (ps :: Nat) = ps
+
+-- Throws an error if greater than the max bound. Returns maxBound if the
+-- incremented result is equal to the max bound.
+moreIdx :: (Eq a, Enum a, Bounded a) => a -> a
+moreIdx a = case result of
+        Nothing -> maxBound
+        Just x -> x
+    where result = succIdx a
+
+-- Throws an error if less than the min bound. Returns minBound if the
+-- decremented result is equal to the min bound.
+lessIdx :: (Eq a, Enum a, Bounded a) => a -> a
+lessIdx a = case result of
+        Nothing -> minBound
+        Just x -> x
+    where result = predIdx a
+
+changed :: (HiddenClockResetEnable dom, Eq a, NFDataX a) => a -> Signal dom a -> Signal dom Bool
+changed x0 x = x ./=. register x0 x
+
+debounce :: forall ps a dom. (Eq a, NFDataX a) 
+         => (HiddenClockResetEnable dom, KnownNat (ClockDivider dom ps)) 
+         => SNat ps 
+         -> a -> Signal dom a -> Signal dom a
+debounce _ start this = regEn start stable this
+    where counter = register (0 :: Index (ClockDivider dom ps)) counterNext
+          counterNext = mux (changed start this) 0 (moreIdx <$> counter)
+          stable = counterNext .==. (pure maxBound)
