66655c99b779a53ff3e755d1d0819c0a77a692c1
Ricky <richardhob@gmail.com>
2024-09-22 09:23:06 -0700

    Start thinking about exercise 3

--- basics/src/Retro/Chapter5.hs
+++ basics/src/Retro/Chapter5.hs
@@ -6,40 +6,43 @@ import Clash.Prelude
 import Retro.Util
 import Retro.SevenSegment
 
+encode10 :: Unsigned 4 -> Vec 7 Bool
+encode10 n = unpack $ case n of
+    --        abcdefg
+    0x0 -> 0b01111110
+    0x1 -> 0b00110000
+    0x2 -> 0b01101101
+    0x3 -> 0b01111001
+    0x4 -> 0b00110011
+    0x5 -> 0b01011011
+    0x6 -> 0b01011111
+    0x7 -> 0b01110000
+    0x8 -> 0b01111111
+    0x9 -> 0b01111011
+    _   -> 0b00000000
+
 -- Create Switches for testing
 --
 -- > let x = (toActive <$> (repeat False)) :: Vec 8 (Active High)
 
--- 1. Given the 8 bit input from the switches, display them on three 7-segment
---    displays in decimal
--- 2. Omit leading zeroes in the decimal version
--- 3. At the press of a pushbutton, start and display a countdown (in seconds)
+-- Exercises
+-- ---------
+--
 -- 4. Digital stopwatch: one pushbutton to start/stop, one to reset to 0. For
 -- extra nicness, compute minutes from the seconds and flash the decimal point
 -- between the minutes and the seconds at half second interval
 
--- 1. Given an 8 bit switches input, display them on three 7 segment displays
+-- Exercise 1
+-- ---------
+-- Given an 8 bit switches input, display them on three 7 segment displays
 --
 -- This is tricky because we have to take the switches and convert it into a
 -- base 10 number to encode (we can't just use Hex to Binary encoding)
+--
 exercise1 :: (HiddenClockResetEnable dom, _)
           => Signal dom (Vec 8 (Active High))
-          -> Signal System (SevenSegment 3 High Low Low)
+          -> Signal dom (SevenSegment 3 High Low Low)
 exercise1 switches = let
-    encode10 :: Unsigned 4 -> Vec 7 Bool
-    encode10 n = unpack $ case n of
-        0x0 -> 0b01111110
-        0x1 -> 0b00110000
-        0x2 -> 0b01101101
-        0x3 -> 0b01111001
-        0x4 -> 0b00110011
-        0x5 -> 0b01011011
-        0x6 -> 0b01011111
-        0x7 -> 0b01110000
-        0x8 -> 0b01111111
-        0x9 -> 0b01111011
-        _   -> 0b00000000
-
     digits :: forall dom. (HiddenClockResetEnable dom) 
            => Signal dom (Vec 8 (Active High)) -> Signal dom (Vec 3 (Maybe (Unsigned 4)))
     digits s = doWork <$> s
@@ -56,3 +59,73 @@ exercise1 switches = let
     toSegments x = (encode10 x, False)
 
     in driveSS toSegments (digits switches)
+
+-- Exercise 2
+-- ---------
+-- Omit leading zeroes in the decimal version
+--
+-- Tricky! We could possibly change up the `encode10` to validate our input ...
+-- Actually, we can just update our `digits` definition to check if the `tens`
+-- or `hundreds` is 0
+exercise2 :: (HiddenClockResetEnable dom, _)
+          => Signal dom (Vec 8 (Active High))
+          -> Signal dom (SevenSegment 3 High Low Low)
+exercise2 switches = let
+    digits :: forall dom. (HiddenClockResetEnable dom) 
+           => Signal dom (Vec 8 (Active High)) -> Signal dom (Vec 3 (Maybe (Unsigned 4)))
+    digits s = doWork <$> s
+        where doWork :: Vec 8 (Active High) -> Vec 3 (Maybe (Unsigned 4))
+              doWork sw = m_hundreds :> m_tens :> ones :> Nil
+                  where value    = pack sw
+                        ones     = Just (to4 $ value `mod` 10)
+
+                        tens     = to4 $ value `div` 10
+                        m_tens   = if tens == 0 then Nothing else (Just tens)
+
+                        hundreds = to4 $ value `div` 100
+                        m_hundreds = if hundreds == 0 then Nothing else (Just hundreds)
+
+    to4 :: BitVector 8 -> Unsigned 4
+    to4 = bitCoerce . truncateB 
+
+    toSegments x = (encode10 x, False)
+
+    in driveSS toSegments (digits switches)
+
+-- Exercise 3
+-- ----------
+-- At the press of a pushbutton, start and display a countdown (in seconds)
+--
+-- OK COOL! So here we have to take exercise 2, and update it to display the
+-- value from a counter instead of from some switches (which is so cool).
+exercise3 :: (HiddenClockResetEnable dom, _)
+          => Signal dom (SevenSegment 3 High Low Low)
+exercise3 = let 
+    digits :: forall dom. (HiddenClockResetEnable dom) 
+           => Signal dom (Vec 8 (Active High)) -> Signal dom (Vec 3 (Maybe (Unsigned 4)))
+    digits s = doWork <$> s
+        where doWork :: Vec 8 (Active High) -> Vec 3 (Maybe (Unsigned 4))
+              doWork sw = m_hundreds :> m_tens :> ones :> Nil
+                  where value    = pack sw
+                        ones     = Just (to4 $ value `mod` 10)
+
+                        tens     = to4 $ value `div` 10
+                        m_tens   = if tens == 0 then Nothing else (Just tens)
+
+                        hundreds = to4 $ value `div` 100
+                        m_hundreds = if hundreds == 0 then Nothing else (Just hundreds)
+
+    to4 :: BitVector 8 -> Unsigned 4
+    to4 = bitCoerce . truncateB 
+
+    toSegments x = (encode10 x, False)
+
+    -- Copied from debounce kinda
+    countDown :: forall ps n dom. (HiddenClockResetEnable dom, KnownNat (ClockDivider dom ps), KnownNat n, _)
+              => SNat ps -> SNat n -> Signal dom (Vec 8 (Active High))
+    countDown _ n = let
+        counter = register (0 :: Index (ClockDivider dom ps)) (rollover <$> counter)
+        s = regEn (snatToNum n) ((0 ==) <$> counter) (s - 1)
+        in s
+
+    in driveSS toSegments (digits (countDown @(Seconds 1) @(10)))
